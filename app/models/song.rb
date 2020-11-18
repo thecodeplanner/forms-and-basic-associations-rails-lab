d98af69c838a874a54a4c086958a87a3c6014780
thecodeplanner <thecodeplanner@gmail.com>
2020-11-18 16:53:57 -0500

    commit tree 0a8c9c78d13b4b3d46d777be82a2b389b7299251

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,34 @@
 class Song < ActiveRecord::Base
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes 
   # add associations here
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+    self.genre ? self.genre.name : nil 
+  end
+
+  def note_ids=(ids)
+    ids.each do |id|
+      note = Notes.find(id)
+      self.notes << note
+    end
+  end
+
+  def note_ids
+    self.notes
+  end
+
 end
